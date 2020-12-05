f44fe0c04ae215236e00884790cca36b2d1a9653
Cloudan29 <guitarman.dan29@gmail.com>
2020-12-04 23:05:01 -0500

    code reuse

--- day03.py
+++ day03.py
@@ -4,48 +4,36 @@ slope = []
 for line in inp:
     slope.append(line.strip())
 
+MOD_NUMBER = len(slope[0])
 
-def part1():
+
+def count_trees(width_increment, height_increment):
     num_trees = 0
     width = 0
     height = 0
-    mod_number = len(slope[0])
 
     while height < len(slope) - 1:
-        width = (width + 3) % mod_number
-        height += 1
+        width = (width + width_increment) % MOD_NUMBER
+        height += height_increment
 
         if slope[height][width] == '#':
             num_trees += 1
     
     return num_trees
+    
+
+def part1():
+    return count_trees(3, 1)
+
 
 def part2():
-
-    num_trees = [0, 0, 0, 0, 0]
-    path_number = -1
-    slope_paths = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
-    mod_number = len(slope[0])
-
-    for slope_path in slope_paths:
-        path_number += 1
-        width = 0
-        height = 0
-
-        while height < len(slope) - 1:
-            width = (width + slope_path[0]) % mod_number
-            height += slope_path[1]
-
-            if slope[height][width] == '#':
-                num_trees[path_number] += 1
+    num_trees = [count_trees(1, 1), count_trees(3, 1), count_trees(5, 1), count_trees(7, 1), count_trees(1, 2)]
 
     answer = 1
     for tree_count in num_trees:
         answer *= tree_count
         
     return answer
-
-
 
 
 print(part1())
