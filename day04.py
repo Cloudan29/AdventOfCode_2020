51b36ec7679a9725317e7eb2fa37ce9921f27340
Cloudan29 <guitarman.dan29@gmail.com>
2020-12-05 01:31:34 -0500

    day4 complete

--- day04.py
+++ day04.py
@@ -1,35 +1,65 @@
+import re
 inp = open("inputs/day04.txt")
 
-def is_valid_passport(passport_keys, needed_keys):
-    if (not 'cid' in passport_keys and len(passport_keys) == len(needed_keys)) or (len(passport_keys) == len(needed_keys) + 1):
+passports = []
+for line in inp:
+    passports.append(line)
+
+NUM_NEEDED_KEYS = 7
+
+def keys_present(passport_keys):
+    if (not 'cid' in passport_keys and len(passport_keys) == NUM_NEEDED_KEYS) or (len(passport_keys) == NUM_NEEDED_KEYS + 1):
         return True
 
-def part1():
+    return False
+
+def is_valid_passport(passport):
+    valid_eye_colours = ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+    num_valid_fields = 0
+
+    if passport['byr'].isnumeric() and int(passport['byr']) >= 1920 and int(passport['byr']) <= 2002:
+        num_valid_fields += 1
+    if passport['iyr'].isnumeric() and int(passport['iyr']) >= 2010 and int(passport['iyr']) <= 2020:
+        num_valid_fields += 1
+    if passport['eyr'].isnumeric() and 2030 >= int(passport['eyr']) >= 2020 and int(passport['eyr']) <= 2030:
+        num_valid_fields += 1
+    if passport['hgt'][-2:] == 'cm' and int(passport['hgt'][:-2]) >= 150 and int(passport['hgt'][:-2]) <= 193:
+        num_valid_fields += 1
+    elif passport['hgt'][-2:] == 'in' and int(passport['hgt'][:-2]) >= 59 and int(passport['hgt'][:-2]) <= 76:
+        num_valid_fields += 1
+    if re.fullmatch('#[0-9a-f]{6}', passport['hcl']):
+        num_valid_fields += 1
+    if passport['ecl'] in valid_eye_colours:
+        num_valid_fields += 1
+    if re.fullmatch('[0-9]{9}', passport['pid']):
+        num_valid_fields += 1
+        
+    return num_valid_fields == 7
+
+def full_solution():
     num_valid = 0
-    needed_keys = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
-    seen = []
+    num_fully_valid = 0
+    seen = {}
 
-    for line in inp:
-        if line == '\n':
-            if is_valid_passport(seen, needed_keys):
+    for passport in passports:
+        if passport == '\n':
+            if keys_present(seen.keys()):
+                if is_valid_passport(seen):
+                    num_fully_valid += 1
                 num_valid += 1
-
-            seen = []
+            
+            seen = {}
             continue
 
-        keyval_pairs = line.split(' ')
+        keyval_pairs = passport.split(' ')
         for keyval in keyval_pairs:
-            seen.append(keyval.split(':')[0])
+            seen[keyval.split(':')[0]] = keyval.split(':')[1].strip()
 
-    # Off by one
-    if is_valid_passport(seen, needed_keys):
+    if keys_present(seen.keys()):
+        if is_valid_passport(seen):
+            num_fully_valid += 1
         num_valid += 1
 
-    seen = []
-    return num_valid
+    return num_valid, num_fully_valid
 
-
-
-
-print(part1())
-
+print(full_solution())
